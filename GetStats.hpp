cf3861ddee3059601200618bb28593aca5dd12c4
smandava0512 <smandava1@ufl.edu>
2025-03-28 00:15:46 +0000

    q1 attempt1

--- GetStats.hpp
+++ GetStats.hpp
@@ -14,7 +14,46 @@ public:
     }
 
     int* getStats() const{
-        //write your function here!
+        int* results = new int[3];
+
+        //min
+        results[0] = 0;
+        //max
+        results[1] = 0;
+        //mean
+        results[2] = 0;
+
+        if(head == nullptr){
+            return &results[0];
+        }
+
+        int min = head->data;
+        int max = 0;
+        int count = 0;
+        int sum = 0;
+
+        Node *temp = head;
+
+        while(temp != nullptr){
+
+            if(temp->data > max){
+                max = temp->data;
+            }
+            if(temp->data < min){
+                min = temp->data;
+            }
+            count++;
+            sum += temp->data;
+            
+        }
+
+        //min
+        results[0] = min;
+        results[1] = max;
+        results[2] = sum/count;
+
+        return &results[0];       
+
     }
     
 
