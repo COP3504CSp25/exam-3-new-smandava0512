b8465769d7c1e8ec10d938f87315569cc5581443
smandava0512 <smandava1@ufl.edu>
2025-03-28 01:35:26 +0000

    fix getstats

--- GetStats.hpp
+++ GetStats.hpp
@@ -45,6 +45,7 @@ public:
             count++;
             sum += temp->data;
             
+            temp = temp->next;
         }
 
         //min
--- TrafficData.hpp
+++ TrafficData.hpp
@@ -7,7 +7,24 @@
 #include <sstream>
 
 std::map<std::string, int> loadTrafficData(const std::string& filename) {
-  //implement your function here!
+
+  //create map
+  std::map<std::string, int> m;
+
+  //load traffic data file
+  std::ifstream file(filename);
+  std::string key;
+  int value = 0;
+
+  while(file.is_open()){
+    file.read(&key, sizeof(char) * 3);
+    file.read(&value, sizeof(int));
+
+    //if reach end of file
+    file.close();
+  }
+
+
 }
 
 void updateTrafficData(const std::string& filename, std::map<std::string, int>& dataMap) {
