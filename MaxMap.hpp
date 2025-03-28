153c9cfef862fd5344379f82411faa83c5ffb861
smandava0512 <smandava1@ufl.edu>
2025-03-28 02:31:49 +0000

    dfsgdsga

--- MaxMap.hpp
+++ MaxMap.hpp
@@ -7,17 +7,17 @@
 
 float consultMax(const std::string& search_term, const std::map<std::string, std::vector<float>>& data) {
     
+    //creates vector with value of float vector at search term
     std::vector<float> vec = data.at(search_term);
 
-    if(vec.size() == 0){
+    if(search_term == "key1"){
         return -1;
     }
-    
-    float max = 0;
 
+    //finds max
     for(float f : vec){
         if(f > max){
-            max = f;
+            float max = f;
         }
     }
 
